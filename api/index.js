721d58dd9c8d93ba55adc092b60e80cb7b20e05e
Oliver Leaver-Smith <oliver.leaver-smith@skybettingandgaming.com>
2018-06-20 14:59:53 +0100

    add new command

--- api/index.js
+++ api/index.js
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import weather from './weather';
+import start from './start';
 
 export default function() {
     let api = Router();
 
     api.use('/weather', weather);
+    api.use('/start', start);
 
     // perhaps expose some API metadata at the root
     api.get('/', (req, res) => {
---
+++ api/start.js
@@ -0,0 +1,37 @@
+import { Router } from 'express';
+
+const router = Router();
+
+router.get('/', function(req, res) {
+    const data = getPayLoad(req);
+    const response = {
+        "text": "Firedrill *start*",
+        "attachments": [{
+            "text": `Dummy GET response. Only you can see this ${data.user_name}`
+        }]
+    };
+    res.send(response);
+});
+
+router.post('/', function(req, res) {
+    const data = getPayLoad(req);
+    const response = {
+        "response_type": "in_channel",
+        "text": "Firedrill *start*",
+        "attachments": [{
+            "text":`Thanks for asking ${data.user_name}. Hi everyone! :wave:`
+        }]
+    };
+    res.send(response);
+});
+
+/*
+    The following data is available (see https://api.slack.com/slash-commands)
+    token, team_id, team_domain, channel_id, channel_name, user_id, user_name,
+    command, text, response_url
+*/
+const getPayLoad = req => {
+    return req.method === 'GET' ? req.query : req.body;
+}
+
+module.exports = router;
